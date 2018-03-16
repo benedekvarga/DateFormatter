0645ea3dc601e6a35cd866f1f73e5629dddd4188
Benedek Varga <vargabenedek92@gmail.com>
2018-03-16 11:40:04 +0100

    DateFormatter added

--- DateFormatter/Utility/DateFormatter.swift
+++ DateFormatter/Utility/DateFormatter.swift
@@ -7,3 +7,64 @@
 //
 
 import Foundation
+
+extension Date {
+
+    /// Returns date and time in format `yyyy.MM.dd - hh:mm:ss`.
+    var full: String {
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: self)
+        let month = calendar.component(.month, from: self)
+        let day = calendar.component(.day, from: self)
+        let hour = calendar.component(.hour, from: self)
+        let minutes = calendar.component(.minute, from: self)
+        let sec = calendar.component(.second, from: self)
+
+        return "\(year).\(month).\(day) - \(hour):\(minutes):\(sec)"
+    }
+
+    /// Returns time in format `hh:mm:ss`.
+    var time: String {
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: self)
+        let minutes = calendar.component(.minute, from: self)
+        let sec = calendar.component(.second, from: self)
+
+        return "\(hour):\(minutes):\(sec)"
+    }
+
+    /// Returs date in format `yyyy.MM.dd`.
+    var date: String {
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: self)
+        let month = calendar.component(.month, from: self)
+        let day = calendar.component(.day, from: self)
+
+        return "\(year).\(month).\(day)"
+    }
+
+    /// Returs date in format `dd.MM.yyyy`.
+    var dateReversed: String {
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: self)
+        let month = calendar.component(.month, from: self)
+        let day = calendar.component(.day, from: self)
+
+        return "\(day).\(month).\(year)"
+    }
+
+    /// Returns date and time in ISO 8601 format `yyyy-MM-dd'T'hh:mm:ss`.
+    var iso8601: String {
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: self)
+        let month = calendar.component(.month, from: self)
+        let day = calendar.component(.day, from: self)
+        let hour = calendar.component(.hour, from: self)
+        let minute = calendar.component(.minute, from: self)
+        let sec = calendar.component(.second, from: self)
+
+        return "\(year)-\(month)-\(day)'T'\(hour):\(minute):\(sec)"
+    }
+
+}
+
--- DateFormatter/ViewController.swift
+++ DateFormatter/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        print(Date().full)
+        print(Date().date)
+        print(Date().time)
     }
 
     override func didReceiveMemoryWarning() {
